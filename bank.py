bcb7e0a077a82508550f3cfa2b2cb57870c0aee4
Ciaran Murphy <ciaranmurphy10@gmail.com>
2024-02-11 19:56:45 +0000

    Added comments

--- bank.py
+++ bank.py
@@ -1,12 +1,25 @@
-amount1 = input("Enter amount1(in cent): ")
+# Request the user to enter an amount in cent and save it to the variable amount1. 
+# This variable will be of type string, even if the user types an integer.
+amount1 = input("Enter amount1(in cent): ") 
+
+# Try to convert the amount to type int and save it to the variable amount1_int. 
+amount1_int = int(amount1)
+
+# Request the user to enter an amount in cent and save it to the variable amount2. 
+# This variable will be of type string, even if the user types an integer.
 amount2 = input("Enter amount2(in cent): ")
 
-amount1_int = int(amount1)
-amount1_int = int(amount1)
+# Try to convert the amount to type int and save it to the variable amount2_int. 
+amount2_int = int(amount2)
 
-sum_in_cent = amount1_int + amount1_int 
+# Add the two integer amounts together and save the sum to the variable sum.
+sum = amount1_int + amount2_int 
 
-sum_euro_part = sum_in_cent // 100
-sum_cent_part = sum_in_cent % 100
+# Use floor division to find the euro amount of the ent amount. 
+sum_euro_part = sum // 100
 
+# Use modulo to find the cent amount of the cent amount.
+sum_cent_part = sum % 100
+
+# Use f strings to print the euro and cent amount in a human readable format. 
 print(f"The sum of these is €{sum_euro_part}.{sum_cent_part}")
