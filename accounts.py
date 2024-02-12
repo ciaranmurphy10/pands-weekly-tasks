c3ca169f327e314c8423a5fb76a597bde41965a0
Ciaran Murphy <ciaranmurphy10@gmail.com>
2024-02-12 11:57:11 +0000

    Added functionality for acc numbers of varying lengths.

--- accounts.py
+++ accounts.py
@@ -1,11 +1,15 @@
-# Request a 10 digit account number from the user..
-acc_no = input("Please enter a 10 digit account number: ")
+# Request an account number from the user. The account number could be of any length.
+# We'll assume that only the account number is entered, and no other text such as spaces or "My account number is...", etc.
+acc_no = input("Please enter your account number: ")
 
 # Since strings are an immutable data type, we'll convert the account number into a list of characters using the list() function. 
 acc_no_list = list(acc_no)
 
-# Next we'll use a for loop to cycle through the first 6 elements of the list and replace them with "X"
-for i in range(0,6): 
+# Since account numbers of varying lengths may be entered, we'll use the len() function to check the how many elements are in the list.
+acc_no_len = len(acc_no_list) 
+
+# Next we'll use a for loop to cycle through the elements of the list, stopping four elements short of the end, and replace them with "X".
+for i in range(0, acc_no_len-4): 
     acc_no_list[i] = "X"
 
 # We can use the .join() method to convert the list back into a string.
