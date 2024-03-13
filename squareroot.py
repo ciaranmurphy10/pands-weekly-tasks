0fcc188d8d5353e1c8f50fbba7da5a3f7dd83ee3
Ciaran Murphy <ciaranmurphy10@gmail.com>
2024-03-13 22:27:07 +0000

    Updated squareroot.py, it is working now.

--- squareroot.py
+++ squareroot.py
@@ -2,7 +2,7 @@
 # The first argument is the number which you want to find the square root of. 
 # The second is the acceptable error. 
 
-def sqrt(number, error, guess = 1):
+def sqrt(number, error = 0.01, guess = 1):
   """
   """
 
@@ -10,7 +10,17 @@ def sqrt(number, error, guess = 1):
   # If a guess is not supplied, the default value of 1 will be used. 
 
   sqrt_est = guess
+  print(f"First guess: {sqrt_est}")
+  print(f"First error: {(number - (sqrt_est ** 2))}")
 
-  while number - (sqrt_est ** 2) > error:
-    sqrt_est = 0.5 * (sqrt_est + (number / sqrt_est)))
+  while abs(number - (sqrt_est ** 2)) > error:
+    sqrt_est = 0.5 * (sqrt_est + (number / sqrt_est))
 
+    print(f"Next guess: {sqrt_est}")
+    print(f"Next error: {(number - (sqrt_est ** 2))}")
+
+  print(f"Final guess: {sqrt_est}")
+
+  return sqrt_est
+
+x = sqrt(9, guess = 2.9)
