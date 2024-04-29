6186fd6ca52d217b8d0cd5ff389cb4f2b1ce39ef
Ciaran Murphy <ciaranmurphy10@gmail.com>
2024-04-29 23:02:30 +0100

    Updated comments and variable names.

--- es.py
+++ es.py
@@ -1,18 +1,19 @@
 import sys
 
-def count_es(file_name):
+# Define a function called count_es() which accepts one argument.
+def count_es(file_path):
   """
-  This function will take a text file and count the number of e's in it. 
+  This function will take a path to a text file and count the number of e's in it. 
 
   Args:
-    file_name (str): The name of the text file. It is assumed that this file will be in the same folder as the script. 
+    file_path (str): The path to the text file. 
 
   Returns:
     int: The count of the number of e's in the text file. 
   """
 
   # Open the file and assign it to the variable file. 
-  with open(file_name, "r") as file:
+  with open(file_path, "r") as file:
 
     # Read the text in the file, and assign it to the variable file_text. 
     # This will be a string. 
@@ -31,10 +32,10 @@ def count_es(file_name):
     return num_es
 
 # The sys module can be used to pass arguments when running a script. 
-# We will assume that only two arguments are passed when running this script, the python script name and the text file name in that order. 
+# We will assume that only two arguments are passed when running this script, the python script name and the path to a text file in that order. 
 # sys.argv will contain a list of arguments passed. 
-# We'll use sys.argv[1] to index the second item in the list, which is our text file name. 
-file_name = sys.argv[1]
+# We'll use sys.argv[1] to index the second item in the list, which is our text file path. 
+file_path = sys.argv[1]
 
-# Run the count_es() function, taking the supplied file as an argument and print the result. 
-print(count_es(file_name))
+# Run the count_es() function, taking the supplied user path as an argument and print the result. 
+print(count_es(file_path))
